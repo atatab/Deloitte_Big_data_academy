f9048e43034c1a765c1b9e260a16965fe06281d1
Leandro Cardozo Alves <lca000>
2022-01-23 16:26:54 -0300

    restrict CRUD operations to Authenticated users only

--- backend_chronos_agency/siteDataApi/views.py
+++ backend_chronos_agency/siteDataApi/views.py
@@ -1,56 +1,69 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView, UpdateAPIView
+from rest_framework.permissions import IsAuthenticated
 from siteDataApi.serializers import ServiceSerializer, TeamMemberSerializer, BlogPostSerializer
 from siteDataApi.models import Service, TeamMember, BlogPost
 
 #Service CRUD
 class ListServiceAPIView(ListAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
 
 class CreateServiceAPIView(CreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
 
 class DestroyServiceAPIView(DestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
 
 class UpdateServiceAPIView(UpdateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
 
 #TeamMember CRUD
 class ListTeamMemberAPIView(ListAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = TeamMember.objects.all()
     serializer_class = TeamMemberSerializer
 
 class CreateTeamMemberAPIView(CreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = TeamMember.objects.all()
     serializer_class = TeamMemberSerializer
 
 class DestroyTeamMemberAPIView(DestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = TeamMember.objects.all()
     serializer_class = TeamMemberSerializer
 
 class UpdateTeamMemberAPIView(UpdateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = TeamMember.objects.all()
     serializer_class = TeamMemberSerializer
 
 #BlogPost CRUD
 class ListBlogPostAPIView(ListAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
 
 class CreateBlogPostAPIView(CreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
 
 class DestroyBlogPostAPIView(DestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
 
 class UpdateBlogPostAPIView(UpdateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
 
